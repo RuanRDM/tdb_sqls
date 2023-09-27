9d8c32477ac304ba0089e5eb361862cca2c5551a
RuanRDM <ruan_delatorre@hotmail.com>
2023-09-27 09:24:35 -0300

    att

--- postgis_raster.sql
+++ postgis_raster.sql
@@ -2,3 +2,7 @@ CREATE EXTENSION postgis_topology;
 
 SET postgis.gdal_enabled_drivers =
 'ENABLE_ALL';
+
+raster2pgsql -s 4326 -I -M *.tif -F -t
+200x200 -a public.raster_data | psql -U
+postgres -d agrogis_v01
